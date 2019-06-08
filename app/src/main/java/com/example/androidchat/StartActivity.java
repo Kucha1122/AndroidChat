4291e339f962f3c4bad34684c6f6cc82a2622801
Kucha <kucha1122@gmail.com>
2019-06-09 01:15:45 +0200

    added start activity

--- app/src/main/java/com/example/androidchat/StartActivity.java
+++ app/src/main/java/com/example/androidchat/StartActivity.java
@@ -2,13 +2,55 @@ package com.example.androidchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
+
+    MaterialButton login,register;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //sprawdzenie czy user jest pusty
+        if(firebaseUser != null) {
+            Intent intent = new Intent(StartActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        //przekierowanie do login activity
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+
+        //przekierowanie do register activity
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+
+            }
+        });
     }
 }
