dd5d675ef7a5d470c432c2fa00de40217e10c89c
Kucha <kucha1122@gmail.com>
2019-06-09 20:15:43 +0200

    some styles and auto-login

--- app/src/main/java/com/example/androidchat/StartActivity.java
+++ app/src/main/java/com/example/androidchat/StartActivity.java
@@ -15,10 +15,27 @@ public class StartActivity extends AppCompatActivity {
 
     Button login,register;
 
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //sprawdzenie czy user jest null
+        if(firebaseUser != null) {
+            Intent intent = new Intent(StartActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
 
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
